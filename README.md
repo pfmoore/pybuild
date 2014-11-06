9c7e1a0b1414017f7c18d0530eccfcbdb52218eb
Paul Moore <p.f.moore@gmail.com>
2014-11-06 13:30:45 +0000

    Initial commit of script and documentation

--- README.md
+++ README.md
@@ -1,4 +1,106 @@
-pybuild
-=======
+pybuild - A Windows build environment for Python
+================================================
 
-Windows build environment for Python
+Overview
+--------
+
+In order to set up a Python build environment on Windows, a number of pieces
+of software are needed, some of which can be tricky to set up correctly. As
+many Python developers use Unix as their primary environment, setting up a
+Windows build environment can be a significant challenge.
+
+The purpose of this project is to simplify that process as much as possible,
+in order to encourage more developers to provide binary wheels for Windows.
+
+Assumptions
+-----------
+
+The build is assumed to be installed on a 64-bit Windows system. This is
+necessary as 32-bit environments cannot build 64-bit software, although the
+converse is true.
+
+The target Windows system is expected to have Powershell installed, as the
+build scripts are written in Powershell. A minimum of Powershell version 2 is
+needed. Windows systems from Windows 7, and Windows Server 2008 R2, come with
+Powershell version 2 or later preinstalled.
+
+Ther target system is assumed to be essentially a clean build of Windows. In
+particular, no C compilers should already be installed. This is simply a matter
+of practicality - the software installers being run typically do not document
+how they react when encountering products that are already installed, and there
+are simply too many possible combinations to test them all. The scripts *may*
+work, but aren't supported in such situations. Patches to improve support for
+such scenarios are welcome, as are reports of cases where the scripts work on
+more complex target systems.
+
+The scripts do not roll back should an error be encountered. As a result, it is
+assumed that should an error occur, the user will have to clean up before
+rerunning the script. The ideal target system is a newly built Windows virtual
+machine, with a snapshot taken before running the build. Recovery is then
+simply a case of restoring the snapshot.
+
+Supported Versions
+------------------
+
+The pybuild project supports building binaries for both 32-bit and 64-bit
+Windows systems, for Python versions 2.7, 3.3 and 3.4.
+
+Python versions older than 2.7 are not supported, and nor is building without
+using setuptools. However, note that many projects that use pure distutils in
+their setup.py will still work, as pip injects setuptools into the build
+process automatically. Only projects that make heavy use of distutils
+customisations, in ways that are incompatible with setuptools, should find this
+an issue in practice.
+
+The reasons for the above limitations are that the "Visual C for Python 2.7"
+compiler package from Microsoft is used by pybuild, and the limitations are
+directly inherited from that package.
+
+Python 3 support, on the other hand, is simply a matter of convenience. Python
+3.3 and 3.4 are the versions commonly in use, and so the scripts have been set
+up to install those versions. Python 3.2 support could be easily added by
+updating the scripts. Python 3.5 and later will be supported when new versions
+are released.
+
+Licensing
+---------
+
+All of the tools installed are free of charge. Checking software licenses is
+the responsibility of the user, but the non-Microsoft software installed is
+under open source compatible licenses, and the Microsoft licenses are believed
+to be acceptable for this type of use. However, the author of the scripts
+takes no responsibility for any license implications involved in running the
+scripts.
+
+Software installed is:
+
+- 7-Zip
+- .NET Framework 4.0
+- Windows SDK 7.1
+- Visual C for Python
+- Visual C 2008 redistributables (required for Python 2.7)
+- Python 2.7, 3.3 and 3.4, 32-bit and 64-bit versions
+
+Customisations
+--------------
+
+The Python 3.3 and 3.4 installations created by the scripts have a
+```sitecustomize``` file installed that enables the SDK compilers whenever
+Python is run. It does this by setting the SDK environment variables in the
+Python process. This may cause unexpected results if the installed Python
+interpreters are used for general purposes rather than for package builds. No
+such issues are expected, and bug reports are welcome if they do, but users
+should be aware that the Python installations are set up first and foremost for
+building extensions.
+
+All the installations have distutils configured to look in a central directory
+for additional libraries. The locations are:
+
+- ```C:\Libraries\Include``` - C header files
+- ```C:\Libraries\Lib32``` - 32-bit object libraries
+- ```C:\Libraries\Lib64``` - 64-bit object libraries
+
+This is intended as a convenience for builds that link to C libraries, but are
+written with an assumption that such libraries have been "installed centrally"
+(which is the norm on Unix, but not on Windows where there is normally no
+central install location).
---
+++ SetupBuildEnv.ps1
@@ -0,0 +1,192 @@
+function CheckIfElevated () {
+    $CurrentLogin = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $secprincipal = New-Object System.Security.Principal.WindowsPrincipal($CurrentLogin)
+    $AdminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $IsAdmin = $secprincipal.IsInRole($AdminRole)
+    if (-not $IsAdmin) {
+        Write-Warning "This script must be run from an elevated shell."
+        Write-Warning "Please restart Powershell using 'Run as Administrator'."
+    }
+    return $IsAdmin
+}
+
+function DownloadFile ($target, $url) {
+    $webclient = New-Object System.Net.WebClient
+    $filename = ($url -split '/')[-1]
+
+    # Make sure $target exists and is an absolute path
+    # (as $webclient.DownloadFile doesn't use the CWD)
+    mkdir $target -ea 0 >$null
+    $target = (Resolve-Path $target).Path
+
+    $filepath = (Join-Path $target $filename)
+    if (Test-Path $filepath) {
+        Write-Host $filepath "already downloaded"
+        return $filepath
+    }
+
+    # Download and retry up to $retry_attempts times in case of network transient errors.
+    Write-Host -fore Yellow "Downloading" $filename
+    Write-Host "Source:" $url
+    $retry_attempts = 3
+    for ($i=0; $i -lt $retry_attempts; $i++) {
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+   }
+   Write-Host "File saved at" $filepath
+   return $filepath
+}
+
+function RunInstaller ([String]$prog, [String]$installer, [String[]]$argv) {
+    if ($installer -like '*.msi') {
+        $argv = ('/I', $installer, $argv)
+        $installer = 'msiexec.exe'
+    }
+    Write-Host -fore Yellow "Installing $prog ($installer $argv)"
+    Start-Process -FilePath $installer -ArgumentList $argv -Wait
+    Write-Host "Completed"
+}
+
+function RunPythonInstaller ($ver) {
+    $v2 = (($ver -split '\.')[0,1] -join '')
+    # Must do 64-bit before 32-bit because of a bug installing 32-bit first
+    RunInstaller "Python $ver (64-bit)" Installers\python-$ver.amd64.msi '/qn',"TARGETDIR=C:\Python$v2-64"
+    RunInstaller "Python $ver (32-bit)" Installers\python-$ver.msi '/qn',"TARGETDIR=C:\Python$v2-32"
+}
+
+$pyversions = ('2.7.8', '3.3.5', '3.4.2')
+
+function DownloadAll () {
+    DownloadFile Installers http://downloads.sourceforge.net/sevenzip/7z920-x64.msi
+    DownloadFile Installers http://download.microsoft.com/download/F/1/0/F10113F5-B750-4969-A255-274341AC6BCE/GRMSDKX_EN_DVD.iso
+    DownloadFile Installers http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe
+    DownloadFile Installers http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi
+    DownloadFile Installers http://download.microsoft.com/download/d/d/9/dd9a82d0-52ef-40db-8dab-795376989c03/vcredist_x86.exe
+    DownloadFile Installers http://download.microsoft.com/download/2/d/6/2d61c766-107b-409d-8fba-c39e61ca08e8/vcredist_x64.exe
+    foreach ($ver in $pyversions) {
+        DownloadFile Installers "https://www.python.org/ftp/python/$ver/python-$ver.msi"
+        DownloadFile Installers "https://www.python.org/ftp/python/$ver/python-$ver.amd64.msi"
+    }
+    DownloadFile Installers https://bootstrap.pypa.io/get-pip.py
+}
+
+function Install7Zip () {
+    RunInstaller "7-Zip" Installers\7z920-x64.msi '/qn'
+}
+
+function UnpackISO () {
+    & "C:\Program Files\7-zip\7z.exe" x -oInstallers\SDK Installers\GRMSDKX_EN_DVD.iso
+}
+
+function InstallSoftware () {
+    RunInstaller ".NET Framework" Installers\dotNetFx40_Full_x86_x64.exe '/q','/norestart'
+    RunInstaller "SDK 7.1" Installers\SDK\setup.exe '-q','-params:ADDLOCAL=ALL'
+    RunInstaller "VC for Python" Installers\VCForPython27.msi '/qn'
+    RunInstaller "VC 2008 redist (32 bit)" Installers\vcredist_x86.exe '/q'
+    RunInstaller "VC 2008 redist (64 bit)" Installers\vcredist_x64.exe '/q'
+}
+
+function InstallPython () {
+    foreach ($ver in $pyversions) {
+        RunPythonInstaller $ver
+    }
+}
+
+function InstallPythonPackages () {
+    foreach ($ver in $pyversions) {
+        $v2 = (($ver -split '\.')[0,1] -join '')
+        foreach ($bits in ('32', '64')) {
+            Write-Host -fore Yellow "Installing pip, setuptools and wheel in Python $ver ($bits bits)"
+            if (-not (Test-Path "C:\Python$v2-$bits\Scripts\pip.exe")) {
+                & "C:\Python$v2-$bits\python.exe" Installers\get-pip.py
+            }
+            & "C:\Python$v2-$bits\python.exe" -m pip install -U pip setuptools wheel
+            Write-Host "Complete"
+        }
+    }
+}
+
+function WriteSiteCustomize () {
+
+    $sitecustomize = @'
+import os
+import platform
+import subprocess
+
+arch = 'x86' if platform.architecture()[0] == '32bit' else 'x64'
+
+batfile = 'C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Bin\\SetEnv.cmd'
+cmd = '"{}" /Release /{} & set'.format(batfile, arch)
+output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, universal_newlines=True)
+
+env = dict([l.split('=',1) for l in output.splitlines() if '=' in l])
+os.environ.update(env)
+os.environ['DISTUTILS_USE_SDK'] = '1'
+'@
+
+    # Only for Python 3.3 and 3.4 as those are the only ones using VS 2010
+    foreach ($v2 in ('33', '34')) {
+        foreach ($bits in ('32', '64')) {
+            Write-Host -fore Yellow "Writing sitecustomize in C:\Python$v2-$bits"
+            $filename = "C:\Python$v2-$bits\Lib\sitecustomize.py"
+            Set-Content $filename $sitecustomize -Encoding ASCII
+        }
+    }
+}
+
+function CreateLibDirectory () {
+    Write-Host -fore Yellow "Creating library directory"
+    mkdir -ea 0 C:\Libraries\Include >$null
+    mkdir -ea 0 C:\Libraries\Lib32 >$null
+    mkdir -ea 0 C:\Libraries\Lib64 >$null
+
+    $distutils_cfg = @"
+[build_ext]
+include_dirs=C:\Libraries\Include
+library_dirs=C:\Libraries\Lib{0}
+"@
+
+    foreach ($ver in $pyversions) {
+        $v2 = (($ver -split '\.')[0,1] -join '')
+        foreach ($bits in ('32', '64')) {
+            Write-Host "Writing distutils.cfg in C:\Python$v2-$bits"
+            $filename = "C:\Python$v2-$bits\Lib\distutils\distutils.cfg"
+            Set-Content $filename ($distutils_cfg -f $bits) -Encoding ASCII
+        }
+    }
+}
+
+function TestInstallation () {
+    del -rec -ea 0 wheelhouse
+    foreach ($ver in $pyversions) {
+        $v2 = (($ver -split '\.')[0,1] -join '')
+        foreach ($bits in ('32', '64')) {
+            Write-Host -fore Yellow "Testing Python $ver ($bits bits)"
+            & "C:\Python$v2-$bits\Scripts\pip.exe" wheel blist
+        }
+    }
+    dir wheelhouse
+}
+
+function Main () {
+    if (-not (CheckIfElevated)) {
+        Write-Host -Fore Red "This script must be run as administrator"
+        return
+    }
+    DownloadAll
+    Install7Zip
+    UnpackISO
+    InstallSoftware
+    InstallPython
+    InstallPythonPackages
+    WriteSiteCustomize
+    CreateLibDirectory
+
+    Write-Host -NoNewline "To test, run "
+    Write-Host -Fore Yellow "TestInstallation"
+}
